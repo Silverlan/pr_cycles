62da0cbb300cded59b070363d5696f18c248288e
Silverlan <silverlann@gmail.com>
2024-06-14 15:01:09 +0200

    build: update submodule "render_raytracing"

--- build_scripts/setup.py
+++ build_scripts/setup.py
@@ -212,7 +212,7 @@ if not Path(rr_tool_root).is_dir():
     git_clone("https://github.com/Silverlan/render_raytracing.git")
 
 os.chdir(rr_tool_root)
-reset_to_commit("f3d0570370992c331fb57815238b0d3d9f8df8a9")
+reset_to_commit("5f30c6dc9dfe3fe645415f357ac9961df96137cf")
 
 additional_build_targets.append("render_raytracing")
 
