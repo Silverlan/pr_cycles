e05608384732a2e7f6936644cb802398ea687dea
Silverlan <silverlan@pragma-engine.com>
2025-07-29 16:39:50 +0200

    build: update submodule "render_raytracing"

--- build_scripts/setup.py
+++ build_scripts/setup.py
@@ -240,7 +240,7 @@ if not Path(rr_tool_root).is_dir():
 	git_clone("https://github.com/Silverlan/render_raytracing.git")
 
 os.chdir(rr_tool_root)
-reset_to_commit("1762ad7804509f795dd1829edf941cc11a9c803e")
+reset_to_commit("c5fa4f26ec4cd48a26c1fd4e3e3c486e10d55150")
 
 ########## Unirender ##########
 unirender_root = root +"/external_libs/util_raytracing"
