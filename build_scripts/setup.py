9520a7aa5c9517e629373ac96a993b1dd46fbd11
Silverlan <silverlan@pragma-engine.com>
2026-01-05 11:12:09 +0100

    build: update submodule "UniRender_cycles"

--- build_scripts/setup.py
+++ build_scripts/setup.py
@@ -12,7 +12,7 @@ os.chdir(deps_dir)
 # These need to match the cycles version that is being used (see build_cycles.py for more information)
 cycles_lib_windows_x64_commit_sha = "cdef408"
 cycles_lib_linux_x64_commit_sha = "eacf548"
-unirender_cycles_release_tag = "2026-01-03"
+unirender_cycles_release_tag = "2026-01-05"
 use_prebuilt_binaries = True
 
 if use_prebuilt_binaries:
