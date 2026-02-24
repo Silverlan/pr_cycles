5f3c896edfd4c567fd06bb12212e20454d25bf24
Silverlan <silverlan@pragma-engine.com>
2026-02-24 21:32:46 +0100

    build: disable fetching prebuilt binaries if --no-build-networking is specified

--- cmake/fetch_prebuilt_binaries.cmake
+++ cmake/fetch_prebuilt_binaries.cmake
@@ -3,4 +3,6 @@ include("${CMAKE_SOURCE_DIR}/cmake/install_helper.cmake")
 set(version "2026-01-18")
 set(base_url "https://github.com/Silverlan/UniRender_Cycles/releases/download")
 
-pr_fetch_prebuilt_binaries("${PRAGMA_DEPS_DIR}" "${base_url}" "${version}" PRIMARY_DIR "${PRAGMA_DEPS_DIR}/cycles")
+if(NOT config.no_build_networking)
+    pr_fetch_prebuilt_binaries("${PRAGMA_DEPS_DIR}" "${base_url}" "${version}" PRIMARY_DIR "${PRAGMA_DEPS_DIR}/cycles")
+endif()
