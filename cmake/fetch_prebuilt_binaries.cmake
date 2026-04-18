efa573beee18e6577c5112f340d36db5078fda27
Silverlan <silverlan@pragma-engine.com>
2026-04-18 14:55:08 +0200

    build: update submodule "unirender_cycles"

--- cmake/fetch_prebuilt_binaries.cmake
+++ cmake/fetch_prebuilt_binaries.cmake
@@ -1,6 +1,6 @@
 include("${CMAKE_SOURCE_DIR}/cmake/install_helper.cmake")
 
-set(version "2026-04-16")
+set(version "2026-04-18")
 set(base_url "https://github.com/Silverlan/UniRender_Cycles/releases/download")
 
 if(NOT config.no_build_networking)
